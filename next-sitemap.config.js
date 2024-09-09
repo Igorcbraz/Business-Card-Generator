2e5b783fb62dad87560369d7f5c7b127345d62f7
Igor <Igorcbraz1@gmail.com>
2024-09-08 21:33:41 -0300

    feat: generate robots.txt to improve seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,18 @@
 
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  exclude: [
+    '/tags/*',
+    '/favicon/*',
+  ],
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ]
+  }
 }
